0c9ed4d57327b413d0bd15cfd149b2c4e7fcdb41
Daniel Zou <danielzou2164@gmail.com>
2025-09-23 22:32:19 -0700

    Revision for version2

--- src/main/java/CLI/TaskManager.java
+++ src/main/java/CLI/TaskManager.java
@@ -1,5 +1,76 @@
 package CLI;
 
+import model.Task;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 public class TaskManager {
 
+    private List<Task> tasks = new ArrayList<Task>();
+
+    public void addTask(int id, String description) {
+        Task newTask = new Task(id, description);
+        tasks.add(newTask);
+        System.out.println("Task added succesfully " + "\n"  +newTask);
+    }
+
+    public void listTask() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks found");
+        } else {
+            for (Task task : tasks) {
+                System.out.println(task);
+            }
+        }
+    }
+
+    public void updateTask(int id, String description) {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks to update");
+        } else {
+            for (Task task : tasks) {
+                if (task.getId() == id) {
+                    task.setDescription(description);
+                    break;
+                }
+            }
+        }
+    }
+    public void deleteTask ( int id){
+        Iterator<Task> iterator = tasks.iterator();
+        while (iterator.hasNext()) {
+            Task task = iterator.next();
+            if(task.getId() == id) {
+                iterator.remove();
+                System.out.println("Task deleted succesfully ");
+                break;
+            }
+        }
+    }
+    public void markTaskAsDone(int id) {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks found");
+        } else {
+            for(Task task : tasks) {
+                if(task.getId() == id) {
+                    task.setStatus("done");
+                    System.out.println("Task marked successfully as done");
+                    break;
+                }
+            }
+        }
+    }
+    public void markTaskInProgress(int id) {
+        if(tasks.isEmpty()) {
+            System.out.println("No tasks found");
+        } else {
+            for(Task task : tasks) {
+                if(task.getId() == id) {
+                    task.setStatus("Task in progress");
+                    System.out.println("Task marked sucessfully as in-progress");
+                    break;
+                }
+            }
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,32 @@
+
+import CLI.TaskManager;
 import model.Task;
 
 public class Main {
     public static void main(String[] args) {
-        Task Task1 = new Task(1, "First CLI Tracker");
-        Task Task2 = new Task(2, "Second CLI Tracker");
-        System.out.println(Task1);
-        System.out.println(Task2);
+        TaskManager manager = new TaskManager();
 
+//        Task Task1 = new Task(1, "First CLI Tracker");
+//        Task Task2 = new Task(2, "Second CLI Tracker");
+//        System.out.println(Task1);
+//        System.out.println(Task2);
+        manager.addTask(1, "First Task");
+        manager.addTask(2, "Second Task");
+
+        System.out.println("\nAll Tasks: ");
+        manager.listTask();
+
+        System.out.println("\nUpdating task: ");
+        manager.updateTask(1, "Learning Java");
+
+        System.out.println("\nChanging task status: ");
+        manager.markTaskAsDone(2);
+
+        System.out.println("\nDeleting task: ");
+        manager.deleteTask(1);
+
+        System.out.println("\nAll Tasks: ");
+        manager.listTask();
     }
 }
+
