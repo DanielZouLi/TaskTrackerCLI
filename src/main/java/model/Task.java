51880934c1f11b1430ecb0c08d52fe9d6d5ac519
Daniel Zou <danielzou2164@gmail.com>
2025-09-23 21:32:32 -0700

    Initial commit

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private int id;
+    private String description;
+    private String status;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.status = "Todo";
+        this.createAt = LocalDateTime.now();
+        this.updateAt = LocalDateTime.now();
+    }
+     public int getId() {
+        return id;
+     }
+     public void setId() {
+        this.id = id;
+     }
+     public String getDescription() {
+        return description;
+     }
+     public void setDescription() {
+        this.description = description;
+        this.updateAt = LocalDateTime.now();
+     }
+     public String getStatus() {
+        return status;
+     }
+     public void setStatus() {
+        this.status = status;
+        this.updateAt = LocalDateTime.now();
+     }
+     public LocalDateTime getCreateAt() {
+        return createAt;
+     }
+     public void setCreateAt() {
+        this.createAt = createAt;
+     }
+     public LocalDateTime getUpdateAt() {
+        return updateAt;
+     }
+     public void setUpdateAt() {
+        this.updateAt = updateAt;
+     }
+     public String toString() {
+        return "ID: " + getId() + "\n" +
+                " | Description: " + getDescription() + "\n" +
+                " | Status: " + getStatus() + "\n" +
+                " | Created At: " +getCreateAt() + "\n" +
+                " | Updated At: " + getUpdateAt() + "\n";
+     }
+}
